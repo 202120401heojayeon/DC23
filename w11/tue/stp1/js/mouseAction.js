07c9673acfd9cf14f838a846eec4852344a02a61
wiintnov <128385368+wiintnov@users.noreply.github.com>
2023-05-23 12:07:44 +0900

    tr

---
+++ w11/tue/stp1/js/mouseAction.js
@@ -0,0 +1,16 @@
+let aCard = document.querySelector(".card");
+console.log(aCard);
+aCard.onclick = (e) => {
+  console.log("target", e.target);
+  console.log("currentTarget", e.currentTarget);
+  e.currentTarget.classList.toggle(
+    "card-selected"
+  );
+};
+
+// let elem = document.getElementById("card1");
+// elem.onclick = () => {
+//   elem.classList.toggle("card-selected");
+// };
+
+// elem.onclick=function(){} -> 위 아래 같은 의미
