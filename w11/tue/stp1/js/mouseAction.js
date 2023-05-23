0346a3ee930b68c98fdc8d862e0ba6e96cc342f4
wiintnov <128385368+wiintnov@users.noreply.github.com>
2023-05-23 12:56:24 +0900

    tru

--- w11/tue/stp1/js/mouseAction.js
+++ w11/tue/stp1/js/mouseAction.js
@@ -14,3 +14,37 @@ aCard.onclick = (e) => {
 // };
 
 // elem.onclick=function(){} -> 위 아래 같은 의미
+
+//stp3
+// let aCard = document.querySelector(".card");
+// console.log(aCard);
+
+// let cards = document.querySelectorAll(".card");
+// console.log(cards);
+// console.log(cards[0]);
+// console.log(cards[1]);
+// console.log(cards[2]);
+// //for 구문 사용
+// // for (let idx = 0; idx < cards.length; idx++) {
+// //   cards[idx].onclick = (e) => {
+// //     e.curretTarget.classList.toggle(
+// //       "card-selected"
+// //     );
+// //   };
+// // }
+
+// //for each 사용
+// cards.forEach((eachCard) => {
+//   eachCard.onclick = function (e) {
+//     e.curretTarget.classList.toggle(
+//       "card-selected "
+//     );
+//   };
+// });
+
+// // let elem = document.getElementById("card1");
+// // elem.onclick = () => {
+// //   elem.classList.toggle("card-selected");
+// // };
+
+// // elem.onclick=function(){} -> 위 아래 같은 의미
